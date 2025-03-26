1a0c2d8e5325172b46728a137e151e8776ccdfe6
Adil surve <180300156+Daiyegamer@users.noreply.github.com>
2025-03-26 18:59:33 -0400

    add files

--- index.php
+++ index.php
@@ -1,16 +1,13 @@
-<?php 
-  include('functions.php');
-  secure();
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Frontend</title>
 </head>
 <body>
-    
+    <h1>This is frontEnd</h1>
+
+    <a href="http://localhost/HTTP5225/week8/admin">Login</a>
 </body>
 </html>
